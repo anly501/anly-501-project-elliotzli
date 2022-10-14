f8d52d3c6142f1170755e0fe14505e5cffe70322
elliotzli <zl516@georgetown.edu>
2022-10-14 19:08:15 -0400

    Update sync-to-cloud.sh

--- sync-to-cloud.sh
+++ sync-to-cloud.sh
@@ -1,7 +1,7 @@
 
 
 # SYNC LOCAL VERSION OF WEBSITE TO GU-DOMAINS SERVER
-rsync -alvr 501-project-website elliotzl@gtown.reclaimhosting.com:/home/elliotzl/public_html/
+rsync -alvr anly-501-project-elliotzli elliotzl@gtown.reclaimhosting.com:/home/elliotzl/public_html/
 
 # PUSH GIT REPO TO THE CLOUD FOR BACKUP
 DATE=$(date +"DATE-%Y-%m-%d-TIME-%H-%M-%S")
@@ -10,3 +10,6 @@ echo "commit message = "$message;
 git add ./; 
 git commit -m $message; 
 git push
+
+git config --global user.email "zl516@georgetown.edu"       
+git config --global user.name "elliotzli"
