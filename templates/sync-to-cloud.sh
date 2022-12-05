2092dff71bd0dd5d18488f10c183886f9b85beea
elliotzli <zl516@georgetown.edu>
2022-12-05 09:49:40 -0500

    gyhj

--- templates/sync-to-cloud.sh
+++ templates/sync-to-cloud.sh
@@ -1,15 +1,4 @@
 
 
-# SYNC LOCAL VERSION OF WEBSITE TO GU-DOMAINS SERVER
+# SYNC LOCAL VERSION OF WEBSITE TO GU-DOMAINS SERVER at home base
 rsync -alvr anly-501-project-elliotzli elliotzl@gtown.reclaimhosting.com:/home/elliotzl/public_html/
-
-# PUSH GIT REPO TO THE CLOUD FOR BACKUP
-DATE=$(date +"DATE-%Y-%m-%d-TIME-%H-%M-%S")
-message="GITHUB-UPLOAD:$DATE";
-echo "commit message = "$message; 
-git add ./; 
-git commit -m $message; 
-git push
-
-git config --global user.email "zl516@georgetown.edu"       
-git config --global user.name "elliotzli"
